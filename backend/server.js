0e34e0b958b8afe61e1e3503922f806a12e73ca3
Dharanipathi Pala <dharanipathipala@gmail.com>
2026-05-20 15:21:25 +0530

    fix db connection with env variables

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,22 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // server parse the Stringfy incoming data to json
 
+//local usage
+// const db = mysql.createConnection({
+//     host : 'localhost',
+//     user : 'root',
+//     password : 'root',
+//     database : 'todo'
+// })
+
 const db = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'root',
-    database : 'todo'
-})
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
 db.connect((err) => {
     if(err){
         console.log("error in connecting db",err);
